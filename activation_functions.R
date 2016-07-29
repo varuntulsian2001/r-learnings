3ef568b237047d7c656d73d5a509c642c92320d4
varun tulsian <vtulsian@walmartlabs.com>
2016-07-29 21:05:16 +0530

    added some functions

--- activation_functions.R
+++ activation_functions.R
@@ -22,4 +22,24 @@ a <- ggplot(zz)+geom_point(aes(y=zz$sigmoid, x=zz$point), color="red")
 
 #relu plot
 a <- a+geom_point(aes(y=zz$relu, x=zz$point), color="blue")
+
+tanh <- function(x){
+  y = (exp(2*x)-1)/(exp(2*x)+1)
+  return(data.frame(tanh=y, point=x))
+}
+
+zz <- merge(zz,tanh(x), by="point")
+
+#tanh plot
+a <- a+ geom_point(aes(y=zz$tanh, x=zz$point), color="black")
+
+sigmoid2 <- function(x){
+  y = 1/(1+exp(-2*x))
+  return(data.frame(sigmoid2=2*y, point=x))
+}
+
+zz <- merge(zz,sigmoid2(x), by="point")
+
+#sigmoid2 plot
+a <- a+ geom_point(aes(y=zz$sigmoid2, x=zz$point), color="cyan")
 a
